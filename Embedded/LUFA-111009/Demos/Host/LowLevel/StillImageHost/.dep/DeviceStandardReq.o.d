77e345d8b22ec20461be1d16c7137bf79358707e
yossymichaeli <yossymichaeli@gmail.com>
2012-03-23 18:12:40 +0000

    Branching for a version with altered bluetooth connectivity mechanism

--- Embedded/LUFA-111009/Demos/Host/LowLevel/StillImageHost/.dep/DeviceStandardReq.o.d
+++
@@ -1,5 +0,0 @@
-../../../../LUFA/Drivers/USB/Core/DeviceStandardReq.o: \
- ../../../../LUFA/Drivers/USB/Core/DeviceStandardReq.c \
- ../../../../LUFA/Drivers/USB/Core/USBMode.h
-
-../../../../LUFA/Drivers/USB/Core/USBMode.h:
--- Embedded/Stabily/.dep/DeviceStandardReq.o.d
+++
@@ -1,5 +0,0 @@
-../LUFA-111009/LUFA/Drivers/USB/Core/DeviceStandardReq.o: \
- ../LUFA-111009/LUFA/Drivers/USB/Core/DeviceStandardReq.c \
- ../LUFA-111009/LUFA/Drivers/USB/Core/USBMode.h
-
-../LUFA-111009/LUFA/Drivers/USB/Core/USBMode.h:
